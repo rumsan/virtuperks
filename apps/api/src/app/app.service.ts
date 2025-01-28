2a68f3582ca9eab5dfb436528f7f244003340fd7
lishu-rumsan <lishu.maharjan@rumsan.net>
2025-01-28 15:39:35 +0545

    feat: ui-setups-updated

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,7 +1,7 @@
-import { EVENTS } from '@app/sdk/events';
-import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
-import { allCaps } from 'node_modules/@app/sdk/dist';
+import {allCaps} from '@app/sdk';
+import {EVENTS} from '@app/sdk/events';
+import {Injectable} from '@nestjs/common';
+import {EventEmitter2} from '@nestjs/event-emitter';
 
 @Injectable()
 export class AppService {
