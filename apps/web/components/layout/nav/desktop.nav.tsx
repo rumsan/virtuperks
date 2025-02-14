064c53ddb9df107f4720f2f9f1e651ed7098e60d
lishu-rumsan <lishu.maharjan@rumsan.net>
2025-02-14 15:40:52 +0545

    feat: nav-css-updated

--- apps/web/components/layout/nav/desktop.nav.tsx
+++ apps/web/components/layout/nav/desktop.nav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Avatar,
   AvatarFallback,
@@ -13,8 +15,21 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function DesktopNav() {
+  const [activeNavBar, setActiveNavBar] = useState("dashboard");
+
+  const handleNavClick = (nav: string) => {
+    setActiveNavBar(nav);
+  };
+
+  const getNavItemClasses = (nav: string) => {
+    return activeNavBar === nav
+      ? "text-[#297AD6] border-b-2 border-[#297AD6]"
+      : "text-[#1E293B] hover:text-[#1e293b]";
+  };
+
   return (
     <header className="border-b bg-white">
       <div className="flex h-14 items-center px-4 gap-8">
@@ -28,50 +43,70 @@ export default function DesktopNav() {
             />
           </Link>
         </nav>
-        <nav className="flex items-center gap-6">
+        <nav className="flex items-center gap-6 h-full">
           <Link
             href="/"
-            className="flex items-center gap-2 text-sm font-normal text-[#1E293B] hover:text-[#1e293b] transition-colors"
+            onClick={() => handleNavClick("dashboard")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("dashboard")}`}
           >
-            <LayoutDashboard size={18} strokeWidth={2.65} color="#334155" />
+            <LayoutDashboard
+              size={18}
+              strokeWidth={2.65}
+              color={`${activeNavBar === "dashboard" ? "#297AD6" : "#334155"}`}
+            />
             Dashboard
           </Link>
           <Link
             href="/departments"
-            className="flex items-center gap-2 text-sm font-normal text-[#1E293B] hover:text-[#1e293b] transition-colors"
+            onClick={() => handleNavClick("departments")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("departments")}`}
           >
-            <Layers size={18} strokeWidth={2.65} color="#334155" />
+            <Layers
+              size={18}
+              strokeWidth={2.65}
+              color={`${activeNavBar === "departments" ? "#297AD6" : "#334155"}`}
+            />
             Department
           </Link>
           <Link
-            href="/participants"
-            className="flex items-center gap-2 text-sm font-normal text-[#1E293B] hover:text-[#1e293b] transition-colors"
+            href="/tasks"
+            onClick={() => handleNavClick("tasks")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("tasks")}`}
           >
-            <Briefcase size={18} strokeWidth={2.65} color="#334155" />
+            <Briefcase
+              size={18}
+              strokeWidth={2.65}
+              color={`${activeNavBar === "tasks" ? "#297AD6" : "#334155"}`}
+            />
             Task Management
           </Link>
           <Link
             href="/treasurer"
-            className="flex items-center gap-2 text-sm font-normal text-[#1E293B] hover:text-[#1e293b] transition-colors"
+            onClick={() => handleNavClick("treasurer")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("treasurer")}`}
           >
-            <Coins size={18} strokeWidth={2.65} color="#334155" />
+            <Coins
+              size={18}
+              strokeWidth={2.65}
+              color={`${activeNavBar === "treasurer" ? "#297AD6" : "#334155"}`}
+            />
             History
           </Link>
         </nav>
-        <div className="ml-auto flex items-center gap-4">
+        <div className="ml-auto flex items-center gap-4 h-full">
           <Link
             href="/tasks"
-            className="flex items-center gap-2 text-sm font-normal text-[#1E293B] hover:text-[#1e293b] transition-colors"
+            onClick={() => handleNavClick("tasks")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("tasks")}`}
           >
             <LayoutList
               size={18}
               strokeWidth={2.65}
-              color="#334155"
-              className=""
+              color={`${activeNavBar === "tasks" ? "#297AD6" : "#334155"}`}
             />
             My Tasks
           </Link>
-          <div className="flex items-center h-10 p-2 bg-[#F1F5F9] rounded-md">
+          <div className="flex items-center h-10 p-2 bg-[#F1F5F9] rounded-md p-2">
             <span className="flex items-center gap-2 font-normal text-[#1E293B] text-sm">
               <Wallet size={18} strokeWidth={2.65} color="#334155" />{" "}
               67wdbb...383hd
