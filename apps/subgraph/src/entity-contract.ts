76846193c2491391acd5ae8dd8e5584ac375a125
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-03-05 13:33:18 +0545

    Initialize subgraph

---
+++ apps/subgraph/src/entity-contract.ts
@@ -0,0 +1,99 @@
+import {
+  PINGED as PINGEDEvent,
+  ParticiantApplied as ParticiantAppliedEvent,
+  TaskAccepted as TaskAcceptedEvent,
+  TaskApproved as TaskApprovedEvent,
+  TaskCompleted as TaskCompletedEvent,
+  TaskCreated as TaskCreatedEvent,
+} from "../generated/templates/EntityContract/EntityContract"
+import {
+  PINGED,
+  ParticiantApplied,
+  TaskAccepted,
+  TaskApproved,
+  TaskCompleted,
+  TaskCreated,
+} from "../generated/schema"
+
+export function handlePINGED(event: PINGEDEvent): void {
+  let entity = new PINGED(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.sender = event.params.sender
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleParticiantApplied(event: ParticiantAppliedEvent): void {
+  let entity = new ParticiantApplied(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.internal_id = event.params.id
+  entity.participant = event.params.participant
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTaskAccepted(event: TaskAcceptedEvent): void {
+  let entity = new TaskAccepted(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.internal_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTaskApproved(event: TaskApprovedEvent): void {
+  let entity = new TaskApproved(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.internal_id = event.params.id
+  entity.approver = event.params.approver
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTaskCompleted(event: TaskCompletedEvent): void {
+  let entity = new TaskCompleted(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.internal_id = event.params.id
+  entity.participant = event.params.participant
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTaskCreated(event: TaskCreatedEvent): void {
+  let entity = new TaskCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.internal_id = event.params.id
+  entity.createdBy = event.params.createdBy
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ apps/subgraph/src/entity-factory.ts
@@ -0,0 +1,28 @@
+import { log } from "@graphprotocol/graph-ts"
+import { EntityTaskManagerCreated as EntityTaskManagerCreatedEvent } from "../generated/EntityFactory/EntityFactory"
+import { EntityTaskManagerCreated } from "../generated/schema"
+import { EntityContract } from "../generated/templates";
+
+
+export function handleEntityTaskManagerCreated(
+  event: EntityTaskManagerCreatedEvent,
+): void {
+  let entity = new EntityTaskManagerCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  )
+  entity.entityTaskManager = event.params.entityTaskManager
+  entity.aclAddress = event.params.aclAddress
+  entity._appId = event.params._appId
+  entity._name = event.params._name
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+   log.debug("EntityTaskManagerCreated: {}", [entity.entityTaskManager.toHexString()]);
+
+  EntityContract.create(event.params.entityTaskManager);
+   log.debug("entityTemplateAdded: {}", [entity.entityTaskManager.toHexString()]);
+}
+
