ef3fa46e0d013a59b0de822afc70587e6af9201e
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-03-24 13:31:44 +0545

    type define in the sdk package

---
+++ packages/sdk/src/types/index.ts
@@ -0,0 +1,8 @@
+export * from './common.type';
+export * from './department.type';
+export * from './entity.type';
+export * from './task.type';
+export * from './taskDetail.type';
+export * from './users.type';
+export * from './participant.type';
+
