52c63b47e3c483b94e9b32d590ea5b129a8cb20b
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-03-30 22:32:21 +0545

    show add department only to Minter role

--- apps/web/components/departments/list/list.card.tsx
+++ apps/web/components/departments/list/list.card.tsx
@@ -1,11 +1,8 @@
 import { useEntityList } from "@/hooks/subgraph/querycall";
 import { PATHS } from "@/routes/paths";
+import hasRole from "@/utils/role";
 import { DepartmentDetails } from "@workspace/sdk/type";
-import {
-  Card,
-  CardDescription,
-  CardTitle,
-} from "@workspace/ui/components/card";
+import { Card, CardDescription, CardTitle } from "@workspace/ui/components/card";
 import { ArrowRight, Coins, Plus, User } from "lucide-react";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
@@ -13,21 +10,24 @@ interface DepartmentListCardProps {
   router: AppRouterInstance;
 }
 
-
 const DepartmentListCard = ({ router }: DepartmentListCardProps) => {
   const getAllEntity = useEntityList();
   const entityList = getAllEntity?.data?.data?.entityTaskManagerCreateds;
- 
+  const hasEntityOwnerRole = hasRole({ 
+    role: process.env.NEXT_PUBLIC_ENTITY_OWNER_ROLE || "" 
+  });
 
   return (
     <div className="grid grid-cols-4 gap-4 w-full p-4">
-      <Card
-        className="w-full flex items-center justify-center text-blue-500 bg-blue-50 border-sm border-primary border-dashed cursor-pointer hover:shadow-lg hover:text-blue-400"
-        onClick={() => router.push(PATHS.DEPARTMENT.ADD)}
-      >
-        <span className="text-center text-base">Add Department</span>
-        <Plus size={24} />
-      </Card>
+      {hasEntityOwnerRole && (
+        <Card
+          className="w-full flex items-center justify-center text-blue-500 bg-blue-50 border-sm border-primary border-dashed cursor-pointer hover:shadow-lg hover:text-blue-400"
+          onClick={() => router.push(PATHS.DEPARTMENT.ADD)}
+        >
+          <span className="text-center text-base">Add Department</span>
+          <Plus size={24} />
+        </Card>
+      )}
       {entityList &&
         entityList.map((department: DepartmentDetails) => {
           return (
--- apps/web/utils/role.ts
+++ apps/web/utils/role.ts
@@ -7,12 +7,13 @@ interface HasRoleProps {
 
 export default function hasRole({ role }: HasRoleProps) {
   const { address } = useAccount();
+  console.log(role,'role')
 
   const { data } = useReadContract({
     address: (process.env.NEXT_PUBLIC_ACCESSMANAGER?.startsWith("0x") ? process.env.NEXT_PUBLIC_ACCESSMANAGER : "") as `0x${string}`,
     abi: AccessManagerABI,
     functionName: "hasRole",
-    args: [process.env.NEXT_PUBLIC_APPID, role, address],
+    args: [process.env.NEXT_PUBLIC_APP_ID, role, address],
   });
 
   return data;
