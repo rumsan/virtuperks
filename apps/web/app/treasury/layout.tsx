a359db94630872720bf31ecdbaac734957b3e7bb
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-04-22 15:32:50 +0545

    remove validation from the app

--- apps/web/app/treasury/layout.tsx
+++ apps/web/app/treasury/layout.tsx
@@ -1,13 +1,12 @@
-import SuperAdminNav from "@/components/layout/nav/super_admin.nav";
-import Validation from "@/providers/validation";
+
 import { PropsWithChildren } from "react";
 
 const layout = ({ children }: PropsWithChildren) => {
   return (
     <div>
-      <Validation role={process.env.NEXT_PUBLIC_SUPER_ADMIN_ROLE || ""}>
-        <SuperAdminNav>{children}</SuperAdminNav>
-      </Validation>
+   {children}
+        
+ 
     </div>
   );
 };
