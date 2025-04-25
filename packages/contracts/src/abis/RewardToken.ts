630fd713e4f09d65f8b21d72bc9505ee0934f8d2
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-04-25 14:14:50 +0545

    Remove contracts folder from the repo

--- packages/contracts/src/abis/RewardToken.ts
+++
@@ -1,4 +0,0 @@
-import RewardTokenArtifacts from '../../build/artifacts/src/contracts/RewardToken.sol/RewardToken.json'
-export const RewardTokenABI = RewardTokenArtifacts.abi
-export const RewardTokenBytecode = RewardTokenArtifacts.bytecode 
-
--- packages/contracts/src/abis/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./AccesManager"
-export * from "./EntityFactoryContract"
-export * from "./EntityTaskManagement"
-export * from "./RewardToken"
