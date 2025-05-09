ecaeea5d8db80001b9d845f72f51c980d3718755
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-05-09 11:27:50 +0545

    Feat: Implement Role-Based Navigation Bar for Multi-Role Users Fixes #88

---
+++ apps/web/components/layout/nav/unified.nav.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@workspace/ui/components/avatar";
+import { ConnectKitButton } from "connectkit";
+import {
+  Coins,
+  Layers,
+  LayoutDashboard,
+  LayoutList,
+  Wallet,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { PropsWithChildren, useState } from "react";
+
+export default function UnifiedNav({ children }: PropsWithChildren) {
+  const [activeNavBar, setActiveNavBar] = useState("dashboard");
+
+  const handleNavClick = (nav: string) => {
+    setActiveNavBar(nav);
+  };
+
+  const getNavItemClasses = (nav: string) =>
+    activeNavBar === nav
+      ? "text-[#297AD6] border-b-2 border-[#297AD6]"
+      : "text-[#1E293B] hover:text-[#1e293b]";
+
+  return (
+    <header className="border-b bg-white">
+      <div className="flex h-14 items-center px-4 gap-8 border-b-2 border-[#E2E8F0]">
+        <nav className="flex items-center justify-center w-[50px] h-full">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/bg/rumsan-logo.png"
+              width={50}
+              height={50}
+              alt="Logo"
+            />
+          </Link>
+        </nav>
+
+        <nav className="flex items-center gap-6 h-full">
+          <Link
+            href="/participants"
+            onClick={() => handleNavClick("participants")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("participants")}`}
+          >
+            <LayoutDashboard size={18} strokeWidth={2.65} />
+            Participants
+          </Link>
+
+          <Link
+            href="/departments"
+            onClick={() => handleNavClick("treasurer-dept")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("treasurer-dept")}`}
+          >
+            <Coins
+              size={18}
+              strokeWidth={2.65}
+              color={activeNavBar === "treasurer-dept" ? "#297AD6" : "#334155"}
+            />
+            Department
+          </Link>
+
+          <Link
+            href="/tasks"
+            onClick={() => handleNavClick("task-mgmt")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("task-mgmt")}`}
+          >
+            <Layers size={18} strokeWidth={2.65} />
+            Task Management
+          </Link>
+        </nav>
+
+        <div className="ml-auto flex items-center gap-4 h-full">
+          <Link
+            href="/task_portal"
+            onClick={() => handleNavClick("task_portal")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("task_portal")}`}
+          >
+            <LayoutList size={18} strokeWidth={2.65} />
+            Tasks Portal
+          </Link>
+
+          <Link
+            href="/task_portal/mine"
+            onClick={() => handleNavClick("my-tasks")}
+            className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses("my-tasks")}`}
+          >
+            <LayoutList size={18} strokeWidth={2.65} />
+            My Tasks
+          </Link>
+
+          <div className="flex items-center h-10 p-2 bg-[#F1F5F9] rounded-md">
+            <span className="flex items-center gap-2 font-normal text-[#1E293B] text-sm">
+              <Wallet size={18} strokeWidth={2.65} color="#334155" />
+              <ConnectKitButton showAvatar={false} theme="auto" />
+            </span>
+          </div>
+
+          <Avatar className="bg-red-400 h-7 w-7">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+
+      <div className="h-[calc(100dvh-60px)] overflow-auto">{children}</div>
+    </header>
+  );
+}
