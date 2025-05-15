a4cbb3ed215efda7f84a5f0bcb0452cafa5cdab7
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-05-15 11:18:55 +0545

    Bug: Fix Token Management nav not working

--- apps/web/components/layout/nav/treasurer.nav.tsx
+++ apps/web/components/layout/nav/treasurer.nav.tsx
@@ -64,7 +64,7 @@ export default function UnifiedNav({ children }: PropsWithChildren) {
             Department
           </Link>
           <Link
-            href="/token"
+            href="/treasurer/token"
             onClick={() => handleNavClick(NavItem.TREASURER_TOKEN)}
             className={`flex items-center gap-2 text-sm font-normal transition-colors h-full p-2 ${getNavItemClasses(NavItem.TREASURER_TOKEN)}`}
           >
