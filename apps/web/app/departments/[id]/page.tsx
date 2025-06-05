d45b1708c46bf43afcfbacc961584c61adfeb798
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-06-05 12:21:23 +0545

    resolve error

--- apps/web/app/departments/[id]/page.tsx
+++ apps/web/app/departments/[id]/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import DepartmentDetails from "@/components/departments/details/details.main";
+// import DepartmentDetails from "@/components/departments/details/details.main";
 import dynamic from "next/dynamic";
 import { useParams, useRouter } from "next/navigation";
 
 function Page() {
   const id = useParams() as { id: string };
-  
+
   const router = useRouter();
-  return (
-    <div>
-      <DepartmentDetails cuid={id} router={router} />;
-    </div>
-  );
+  return <div>{/* <DepartmentDetails cuid={id} router={router} />; */}</div>;
 }
 
 export default dynamic(() => Promise.resolve(Page), {
