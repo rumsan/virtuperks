780281a050592f41106f65f4ea7760c619102a00
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-06-09 17:54:35 +0545

    fix errors

--- apps/web/hooks/subgraph/participant.ts
+++ apps/web/hooks/subgraph/participant.ts
@@ -1,35 +1,35 @@
-// import { useGraphService } from "@/providers/subgraph-provider";
-// import { useQuery } from "@tanstack/react-query";
+import { useGraphService } from "@/providers/subgraph-provider";
+import { useQuery } from "@tanstack/react-query";
 
-// export const useGetTaskListByParticipant = (
-//   participant: string,
-//   skip: boolean = false,
-// ) => {
-//   const { queryService } = useGraphService();
+export const useGetTaskListByParticipant = (
+  participant: string,
+  skip: boolean = false,
+) => {
+  const { queryService } = useGraphService();
 
-//   return useQuery({
-//     queryKey: ["myTask", participant],
-//     queryFn: async () => {
-//       const taskDetail =
-//         await queryService?.getAllTaskByParticipant(participant);
-//       return taskDetail;
-//     },
-//     enabled: !!participant && !skip,
-//   });
-// };
+  return useQuery({
+    queryKey: ["myTask", participant],
+    queryFn: async () => {
+      const taskDetail =
+        await queryService?.getAllTaskByParticipant(participant);
+      return taskDetail;
+    },
+    enabled: !!participant && !skip,
+  });
+};
 
-// export const useGetAllParticipantsByRole = (
-//   role: string,
-//   skip: boolean = false,
-// ) => {
-//   const { queryService } = useGraphService();
+export const useGetAllParticipantsByRole = (
+  role: string,
+  skip: boolean = false,
+) => {
+  const { queryService } = useGraphService();
 
-//   return useQuery({
-//     queryKey: ["participantsByRole", role],
-//     queryFn: async () => {
-//       const participants = await queryService?.getAllParticipantsByRole(role);
-//       return participants;
-//     },
-//     enabled: !!role && !skip,
-//   });
-// };
+  return useQuery({
+    queryKey: ["participantsByRole", role],
+    queryFn: async () => {
+      const participants = await queryService?.getAllParticipantsByRole(role);
+      return participants;
+    },
+    enabled: !!role && !skip,
+  });
+};
