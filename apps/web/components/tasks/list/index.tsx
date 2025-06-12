1c428e03a952ba232cf7ef93b4470d4fb961d7bb
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-06-12 13:26:04 +0545

    dataTablepagination compoenent implemented

--- apps/web/components/tasks/list/index.tsx
+++ apps/web/components/tasks/list/index.tsx
@@ -11,9 +11,12 @@ import {
 } from "@workspace/ui/components/tabs";
 import { Plus } from "lucide-react";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-import { useState } from "react";
+import React, { useState } from "react";
 import ListCardDetails from "./list.card";
 import { DatePickerWithRange } from "./list.date";
+import { useColumns } from "../details/details.column";
+import { ColumnFiltersState, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable, VisibilityState } from "@tanstack/react-table";
+import { DataTablePagination } from "@/components/common/list/list.pagination";
 
 interface TaskListMainProps {
   router: AppRouterInstance;
@@ -21,10 +24,41 @@ interface TaskListMainProps {
 
 export default function TaskListMain({ router }: TaskListMainProps) {
   const [tabStatus, setTabStatus] = useState("active");
+    const [sorting, setSorting] = React.useState<SortingState>([]);
 
   const getAllTask = useGetAllTask();
+  const columns = useColumns();
+    const [columnVisibility, setColumnVisibility] =
+      React.useState<VisibilityState>({});
+ const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+     [],
+   );
+    const [rowSelection, setRowSelection] = React.useState({});
+    const [pagination, setPagination] = React.useState({
+      pageIndex: 0,
+      pageSize: 10,
+    });
 
   const taskList = getAllTask?.data?.data?.taskCreateds;
+  const table = useReactTable({
+      data: taskList || [],
+      columns,
+      onSortingChange: setSorting,
+      onColumnFiltersChange: setColumnFilters,
+      getCoreRowModel: getCoreRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
+      getSortedRowModel: getSortedRowModel(),
+      getFilteredRowModel: getFilteredRowModel(),
+      onColumnVisibilityChange: setColumnVisibility,
+      onRowSelectionChange: setRowSelection,
+      state: {
+        sorting,
+        columnFilters,
+        columnVisibility,
+        rowSelection,
+      },
+    });
+  
  
   return (
     <main className="gap-2 p-2 sm:px-6 sm:py-1 md:gap-8 w-full">
@@ -92,6 +126,13 @@ export default function TaskListMain({ router }: TaskListMainProps) {
               />
             </TabsContent>
           </div>
+          <div className="mt-5 mb-5">
+                    <DataTablePagination
+                      table={table}
+                      setPagination={setPagination}
+                      pagination={pagination}
+                    />
+                  </div>
         </Tabs>
       </div>
     </main>
