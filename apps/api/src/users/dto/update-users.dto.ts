fdb009f70ca96b0b4ba562ca83a1df5b93cbdfa0
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-08-04 15:21:44 +0545

    install validator and transformer dependencies

--- apps/api/src/users/dto/update-users.dto.ts
+++ apps/api/src/users/dto/update-users.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateRewardDto } from './create-users.dto';
+// import { CreateRewardDto } from './create-users.dto';
 
 
-export class UpdateRewardDto extends PartialType(CreateRewardDto) {
+// export class UpdateRewardDto extends PartialType(CreateRewardDto) {
   
-}
+// }
