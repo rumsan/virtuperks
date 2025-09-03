4e5a2256c282c51196664c7f018c3336df2495cb
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-09-03 16:29:58 +0545

    Feat: display the close expired task button for entity owner only

--- apps/web/components/departments/details/details.card.tsx
+++ apps/web/components/departments/details/details.card.tsx
@@ -237,7 +237,7 @@ export default function DepartmentDetailsCard({
             )}
 
             {/* Close Expired Tasks Button */}
-            {getCloseExpiredButton()}
+            {canTransferToken && getCloseExpiredButton()}
           </div>
         </div>
       </div>
