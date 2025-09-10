38ccaaee34eb00d20b3ecc24d86b5065ab89c3ad
ngima-rumsan <nigma.sherpa@rumsan.net>
2025-09-10 14:27:04 +0545

    Fix: minor css changes

--- apps/web/components/departments/details/details.card.tsx
+++ apps/web/components/departments/details/details.card.tsx
@@ -270,18 +270,12 @@ export default function DepartmentDetailsCard({
                             setTimeout(() => setCopiedOwner(null), 2000);
                           }}
                         >
-                          <span
-                            className={`truncate max-w-[200px] transition-colors ${
-                              copiedOwner === owner
-                                ? "text-blue-800 underline"
-                                : "group-hover:text-blue-800 group-hover:underline"
-                            }`}
-                          >
+                          <span className="truncate max-w-[200px] transition-colors text-[#475569]">
                             {owner}
                           </span>
                           <div className="flex items-center transition-colors">
                             {copiedOwner === owner ? (
-                              <span className="text-green-800 font-bold">
+                              <span className="text-green-600 font-bold">
                                 ✔
                               </span>
                             ) : (
