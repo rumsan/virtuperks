5983bcdb4a44f06935d26a71c5d49f6dac64a2bf
Rumsan Office <rumsanoffice@RS-25-01-Mac.local>
2025-10-16 14:16:51 +0545

    refactor token market place

--- apps/web/utils/api.utils.ts
+++ apps/web/utils/api.utils.ts
@@ -1,20 +1,20 @@
-import { QueryClient } from "@tanstack/react-query";
-import { ApiClient } from "@workspace/sdk/clients";
-//import { AppEventManager } from "../event.manger";
+// import { QueryClient } from "@tanstack/react-query";
+// import { ApiClient } from "@workspace/sdk/clients";
+// //import { AppEventManager } from "../event.manger";
 
-const fallbackQueryClient = new QueryClient();
-export function useRemoteClient() {
-  // const { accessToken, appId, clientId } = useRumsanAppStore();
+// const fallbackQueryClient = new QueryClient();
+// export function useRemoteClient() {
+//   // const { accessToken, appId, clientId } = useRumsanAppStore();
 
-  return {
-    apiClient: new ApiClient({
-      baseURL: process.env.NEXT_PUBLIC_API_URL,
-      //   headers: {
-      //     Authorization: `Bearer ${accessToken}`,
-      //     "rs-app-id": appId,
-      //     "rs-client-id": clientId,
-      //   },
-    }),
-    queryClient: fallbackQueryClient,
-  };
-}
+//   return {
+//     apiClient: new ApiClient({
+//       baseURL: process.env.NEXT_PUBLIC_API_URL,
+//       //   headers: {
+//       //     Authorization: `Bearer ${accessToken}`,
+//       //     "rs-app-id": appId,
+//       //     "rs-client-id": clientId,
+//       //   },
+//     }),
+//     queryClient: fallbackQueryClient,
+//   };
+// }
--- packages/sdk/src/clients/redemption.client.ts
+++ packages/sdk/src/clients/redemption.client.ts
@@ -1,63 +1,63 @@
 
-import { Pagination } from '@rumsan/sdk/types';
-import { formatResponse } from '@rumsan/sdk/utils/formatResponse.utils';
-import { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { CreateRedemption, Redemption } from '../types';
+// import { Pagination } from '@rumsan/sdk/types';
+// import { formatResponse } from '@rumsan/sdk/utils/formatResponse.utils';
+// import { AxiosInstance, AxiosRequestConfig } from 'axios';
+// import { CreateRedemption, Redemption } from '../types';
 
-export class RedemptionClient {
-  private _client: AxiosInstance;
-  private _prefix = 'redemption';
+// export class RedemptionClient {
+//   private _client: AxiosInstance;
+//   private _prefix = 'redemption';
 
-  constructor(private apiClient: AxiosInstance) {
-    this._client = apiClient;
-  }
+//   constructor(private apiClient: AxiosInstance) {
+//     this._client = apiClient;
+//   }
 
-  async create(data: CreateRedemption, config?: AxiosRequestConfig) {
-    const response = await this._client.post(`${this._prefix}`, data, config);
-    return response 
-  }
+//   async create(data: CreateRedemption, config?: AxiosRequestConfig) {
+//     const response = await this._client.post(`${this._prefix}`, data, config);
+//     return response 
+//   }
 
-  async list(data?: Pagination, config?: AxiosRequestConfig) {
-    const response = await this._client.get(`${this._prefix}`, {
-      params: data,
-      ...config,
-    });
-    return formatResponse<Redemption[]>(response);
-  }
+//   async list(data?: Pagination, config?: AxiosRequestConfig) {
+//     const response = await this._client.get(`${this._prefix}`, {
+//       params: data,
+//       ...config,
+//     });
+//     return formatResponse<Redemption[]>(response);
+//   }
 
-  async search(
-    params?: Pagination,
-    filters?: any,
-    config?: AxiosRequestConfig,
-  ) {
+//   async search(
+//     params?: Pagination,
+//     filters?: any,
+//     config?: AxiosRequestConfig,
+//   ) {
    
-     const response = await this._client.post(
-      `${this._prefix}/search`,
-      filters,
-      {
-        params,
-        ...config,
-      },
-    );
-    return formatResponse<Redemption[]>(response);
-  }
+//      const response = await this._client.post(
+//       `${this._prefix}/search`,
+//       filters,
+//       {
+//         params,
+//         ...config,
+//       },
+//     );
+//     return formatResponse<Redemption[]>(response);
+//   }
 
   
 
-  async findOne(cuid: string, config?: AxiosRequestConfig) {
-    const response = await this._client.get(`${this._prefix}/${cuid}`, config);
-    return response
-  }
+//   async findOne(cuid: string, config?: AxiosRequestConfig) {
+//     const response = await this._client.get(`${this._prefix}/${cuid}`, config);
+//     return response
+//   }
 
   
-  async delete(cuid: string, config?: AxiosRequestConfig) {
-    const response = await this._client.delete(`${this._prefix}/${cuid}`, config);
-    return response
-  }
-  async update(cuid: string, dto: any, config?: AxiosRequestConfig) {
+//   async delete(cuid: string, config?: AxiosRequestConfig) {
+//     const response = await this._client.delete(`${this._prefix}/${cuid}`, config);
+//     return response
+//   }
+//   async update(cuid: string, dto: any, config?: AxiosRequestConfig) {
 
   
-    const response = await this._client.put(`${this._prefix}/${cuid}`, dto, config);
-    return response
-  }
-}
+//     const response = await this._client.put(`${this._prefix}/${cuid}`, dto, config);
+//     return response
+//   }
+// }
