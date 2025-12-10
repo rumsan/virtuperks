26496427c6b4b13b242eeb922db822e9837cbebc
Pratiksharai-Rumsan <pratiksha.rai@rumsan.net>
2025-12-10 15:00:40 +0545

    mapped entity owners in hook

--- apps/web/hooks/client/participant.lookup.ts
+++ apps/web/hooks/client/participant.lookup.ts
@@ -1,5 +1,6 @@
 import { participantLookupClient } from "@/utils/participant.lookup.api";
 import { useQuery } from "@tanstack/react-query";
+import { useGetEntityOwners } from "../subgraph/entity";
 import { useGetTreasurerWallets } from "../subgraph/treasurer.role.check";
 
 export const useParticipantLookup = () => {
@@ -23,7 +24,7 @@ export const useParticipantLookup = () => {
   };
 };
 
-export const useSelectParticipantLookUp = () => {
+export const useSelectParticipantLookUp = (entityId?: string) => {
   const {
     data,
     isError,
@@ -32,6 +33,27 @@ export const useSelectParticipantLookUp = () => {
   const treasurerRole = process.env.NEXT_PUBLIC_MINTER_ROLE!;
   const { data: treasurerWallets, isLoading: treasurerWalletsLoading } =
     useGetTreasurerWallets(treasurerRole);
+  const { getEntityOwners, statusLoading: ownersLoading } = useGetEntityOwners(
+    entityId || "",
+  );
+
+  const mappedEntityOwners =
+    getEntityOwners?.map((wallet: any) => {
+      if (isError || !data?.data) {
+        return {
+          wallet: wallet,
+          label: wallet,
+        };
+      }
+      const entityOwner = data.data.find(
+        (item: any) => item.address?.toLowerCase() === wallet.toLowerCase(),
+      );
+
+      return {
+        wallet: wallet,
+        label: entityOwner ? entityOwner.name : wallet.account,
+      };
+    }) || [];
 
   const mappedTreasurers =
     treasurerWallets?.map((wallet: any) => {
@@ -57,6 +79,7 @@ export const useSelectParticipantLookUp = () => {
   return {
     name: data?.data || [],
     mappedTreasurers,
+    mappedEntityOwners,
     isLoading: treasurerWalletsLoading || participantLoading,
     hasParticipantError: isError,
     lookupByCuid: (address: string) =>
